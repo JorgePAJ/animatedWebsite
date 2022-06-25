c2c3cbe87b9fa8004226412f0d36fb1478a3ddbd
Jorge Plasencia <66725751+WaberHoruhe@users.noreply.github.com>
2022-06-25 14:00:29 -0600

    feature completed, search in progress, music player upper ready

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -3,8 +3,9 @@ import React from "react";
 function Features({ icon, title }) {
   return (
     <div className="feature flex items-center justify-center flex-col relative text-center mx-12">
-      <div className="icon" />
-      <img src={require(`../img/${icon}.png`)} alt="" className="w-[3rem]" />
+      <div className="icon bg-[#081730] rounded-2xl p-4">
+        <img src={require(`../img/${icon}.png`)} alt="" className="w-[3rem]" />
+      </div>
       <span className="mt-5">{title}</span>
       <span className="text-[#707070] mt-4">
         Nunc elementum, dolor vitae lacinae, nisi nisi, euismod nisi,
---
+++ src/components/MusicPlayer.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+function MusicPlayer() {
+  return (
+    <div className="musicPlayer flex flex-col px-5 w-[100%] border-2 border-black py-3 bg-[#232A43] rounded-xl mt-5">
+      <div className="upperpart flex items-center justify-between">
+        <div className="profile flex">
+          <img
+            src={require("../img/Mask Group 23.png")}
+            alt=""
+            className="w-[2.5rem] h-[2.5rem] border-2 border-white rounded-full"
+          />
+          <div className="details flex flex-col ml-4">
+            <span className="text-[1rem]">Tristam Bone Dry</span>
+            <span className="text-sm text-gray-500">Unknown Artist</span>
+          </div>
+        </div>
+        <div>
+          <img src={require("../img/path.png")} alt="" className="h-[1rem]" />
+        </div>
+      </div>
+
+      <div className="lowerPart">
+            
+
+
+      </div>
+    </div>
+  );
+}
+
+export default MusicPlayer;
